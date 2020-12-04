b26a6eb0d980ebf702ef514124ed494b95325626
Marco Silva <marco.silva1981@gmail.com>
2020-12-04 10:58:43 +0100

    first commit

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,10 +1,15 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TestController extends AbstractController{
-    public function  index(){
-        dd("toto");
+    public function  index($val){
+        $request = Request::createFromGlobals();
+        dd(
+            $request->get('val')
+        
+        );
     }
 }
